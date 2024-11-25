04384e55c91e7b13447805d05efa2f879bb76498
adityagupta <adityasgupta0412@gmail.com>
2024-11-25 14:49:52 +0530

    Initial Commit

---
+++ EmployeeRoster/EmployeeDetailTableViewController.swift
@@ -0,0 +1,115 @@
+
+import UIKit
+
+protocol EmployeeDetailTableViewControllerDelegate: AnyObject {
+    func employeeDetailTableViewController(_ controller: EmployeeDetailTableViewController, didSave employee: Employee)
+}
+
+class EmployeeDetailTableViewController: UITableViewController, UITextFieldDelegate, EmployeeTypeTableViewControllerDelegate{
+    
+    var employeeType : EmployeeType?
+    
+    func employeeTypeTableViewController(_ controller: EmployeeTypeTableViewController, didSelect employeeType: EmployeeType) {
+        self.employeeType = employeeType
+        employeeTypeLabel.text = employeeType.description
+        employeeTypeLabel.textColor = .black
+        updateSaveButtonState()
+    }
+    
+
+    
+
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var dobLabel: UILabel!
+    @IBOutlet var employeeTypeLabel: UILabel!
+    @IBOutlet var saveBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var dobDatePicker: UIDatePicker!
+    
+    var isEditingBirthday = false {
+        didSet{
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
+    
+    weak var delegate: EmployeeDetailTableViewControllerDelegate?
+    var employee: Employee?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateView()
+        updateSaveButtonState()
+    }
+    
+    func updateView() {
+        if let employee = employee {
+            navigationItem.title = employee.name
+            nameTextField.text = employee.name
+            
+            dobLabel.text = employee.dateOfBirth.formatted(date: .abbreviated, time: .omitted)
+            dobLabel.textColor = .label
+            employeeTypeLabel.text = employee.employeeType.description
+            employeeTypeLabel.textColor = .label
+        } else {
+            navigationItem.title = "New Employee"
+        }
+    }
+    
+    private func updateSaveButtonState() {
+        let shouldEnableSaveButton = employeeType != nil
+        saveBarButtonItem.isEnabled = shouldEnableSaveButton
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let name = nameTextField.text else {
+            return
+        }
+        guard let employeeType else {return}
+        
+        let employee = Employee(name: name, dateOfBirth: dobDatePicker.date, employeeType: employeeType)
+        delegate?.employeeDetailTableViewController(self, didSave: employee)
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        employee = nil
+    }
+
+    @IBAction func nameTextFieldDidChange(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath == IndexPath(row: 2, section: 0){
+            if (isEditingBirthday) {
+                return UITableView.automaticDimension
+            }else{
+                return 0
+            }
+            
+        }
+        return UITableView.automaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath == IndexPath(row: 1, section: 0) {
+            isEditingBirthday.toggle()
+            dobLabel.textColor = .label
+            dobLabel.text = dobDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+        }
+    }
+    
+    @IBSegueAction func showEmployeeTypes(_ coder: NSCoder) -> EmployeeTypeTableViewController? {
+        
+        let next = EmployeeTypeTableViewController(coder: coder)
+        
+        next?.delegate = self
+        
+        return next
+        
+    }
+    
+
+}
